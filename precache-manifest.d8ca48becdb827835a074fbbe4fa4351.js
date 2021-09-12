cf3a12a3ec2985d1de902344459681e1a90a6ae1
KaiWei <ga642381@gmail.com>
2021-09-13 07:29:58 +0800

    add submit paper button

---
+++ precache-manifest.d8ca48becdb827835a074fbbe4fa4351.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "386dc960c59c9f6b0bb7a3255a03a638",
+    "url": "/index.html"
+  },
+  {
+    "revision": "772e2cf83529c978e9fc",
+    "url": "/static/css/main.7422a277.chunk.css"
+  },
+  {
+    "revision": "10594a0e540673ff0fc5",
+    "url": "/static/js/2.99f8fa9d.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.99f8fa9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "772e2cf83529c978e9fc",
+    "url": "/static/js/main.bd43eae9.chunk.js"
+  },
+  {
+    "revision": "deaf17d855cbdaf7344d",
+    "url": "/static/js/runtime-main.47788c5d.js"
+  },
+  {
+    "revision": "2ab7ff0a1c469c0d9cdc5c876810d13e",
+    "url": "/static/media/location.2ab7ff0a.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2946a10273c4f2f7dcaf303489a62db9.js"
+  "/precache-manifest.d8ca48becdb827835a074fbbe4fa4351.js"
 );
 
 self.addEventListener('message', (event) => {
