0dd518563a93163f5f2ad8377d369d2a076b8710
KaiWei <ga642381@gmail.com>
2022-02-01 00:37:29 +0800

    update schedule

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import { withStyles, makeStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
+import Paper from "@material-ui/core/Paper";
+import { makeStyles, withStyles } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import React from "react";
 
 const StyledTableCell = withStyles((theme) => ({
   head: {
@@ -20,37 +20,90 @@ const StyledTableCell = withStyles((theme) => ({
 
 const StyledTableRow = withStyles((theme) => ({
   root: {
-    '&:nth-of-type(odd)': {
+    "&:nth-of-type(odd)": {
       backgroundColor: theme.palette.action.hover,
     },
   },
 }))(TableRow);
 
-function createData(name, calories, fat, carbs) {
-  return { name, calories, fat, carbs };
+function createData(time, activity, title) {
+  return { time, activity, title };
 }
 
 function listofname(names) {
-  return <div style={{width: 170, overflowX: "auto"}}>
-    {
-      names.map(name => <p>{name}</p>)
-    }
-  </div>
+  return (
+    <div style={{ width: 170, overflowX: "auto" }}>
+      {names.map((name) => (
+        <p>{name}</p>
+      ))}
+    </div>
+  );
 }
 
 function listoftitle(names) {
-  return <div style={{width: 720, overflowX: "auto"}}>
-    {
-      names.map(name => <p>{name}</p>)
-    }
-  </div>
+  return (
+    <div style={{ width: 720, overflowX: "auto" }}>
+      {names.map((name) => (
+        <p>{name}</p>
+      ))}
+    </div>
+  );
 }
 
-const rows = [
-  createData('TBD', "TBD", "TBD", "TBD"),
-  createData('TBD', "TBD", "TBD", "TBD")
+const section1 = [
+  createData("8:50 - 9:00", "Opening", "Organizers"),
+  createData("9:00 - 9:50", "Keynote 1", "40 mins presentation + 10 mins Q&A"),
+  createData(
+    "9:50 - 11:00",
+    "Invited talks 1 & 2 & 3",
+    "20 mins presentations for each speaker + 10 mins Q&A"
+  ),
 ];
 
+const section2 = [
+  createData(
+    "11:10 - 12:00",
+    "Paper presentation - session 1",
+    "Regular paper (10 mins for each speaker + 10 mins Q&A)"
+  ),
+  createData(
+    "12:00 - 12:50",
+    "Paper presentation - session 2",
+    "Regular paper (10 mins for each speaker + 10 mins Q&A)"
+  ),
+];
+const section3 = [
+  createData(
+    "13:20 - 14:30",
+    "Invited talk 4 & 5 & 6",
+    "20 mins presentations for each speaker + 10 mins Q&A"
+  ),
+  createData(
+    "14:30 - 15:20",
+    "Paper presentation - session 3",
+    "Regular paper (10 mins for each speaker + 10 mins Q&A)"
+  ),
+];
+const section4 = [
+  createData(
+    "15:50 - 16:40",
+    "Keynote 2",
+    "40 mins presentation + 10 mins Q&A "
+  ),
+];
+const section5 = [
+  createData(
+    "17:10 - 18:00",
+    "Invited talks 7 & 8",
+    "20 mins presentations for each speaker + 10 mins Q&A"
+  ),
+  createData(
+    "18:00 - 19:00",
+    "Paper presentation (SUPERB)",
+    "3 papers + wavLM + result presentation (10 mins each) + 10 mins Q&A"
+  ),
+  createData("19:00 - 19:10", "Closing remarks", "Organizers"),
+];
 const useStyles = makeStyles({
   table: {
     minWidth: 700,
@@ -66,20 +119,67 @@ export default function CustomizedTables() {
         <TableHead>
           <TableRow>
             <StyledTableCell>Time (EST)</StyledTableCell>
-            <StyledTableCell align="left">Type</StyledTableCell>
-            <StyledTableCell align="left">Title</StyledTableCell>
-            <StyledTableCell align="left">Presenters</StyledTableCell>
+            <StyledTableCell align="left">Activity</StyledTableCell>
+            <StyledTableCell align="left">
+              Presenters / Presentation title / Note
+            </StyledTableCell>
           </TableRow>
         </TableHead>
+
         <TableBody>
-          {rows.map((row) => (
-            <StyledTableRow key={row.name}>
-              <StyledTableCell align="left">
-                <p style={{width: 80, margin: "auto"}}>{row.name}</p>
-              </StyledTableCell>
-              <StyledTableCell align="left">{row.calories}</StyledTableCell>
-              <StyledTableCell align="left">{row.carbs}</StyledTableCell>
-              <StyledTableCell align="left">{row.fat}</StyledTableCell>
+          {section1.map((row) => (
+            <StyledTableRow key={row.title}>
+              <StyledTableCell align="left">{row.time}</StyledTableCell>
+              <StyledTableCell align="left">{row.activity}</StyledTableCell>
+              <StyledTableCell align="left">{row.title}</StyledTableCell>
+            </StyledTableRow>
+          ))}
+          <StyledTableRow>
+            <StyledTableCell colspan="3" align="center">
+              10-min Break
+            </StyledTableCell>
+          </StyledTableRow>
+          {section2.map((row) => (
+            <StyledTableRow key={row.title}>
+              <StyledTableCell align="left">{row.time}</StyledTableCell>
+              <StyledTableCell align="left">{row.activity}</StyledTableCell>
+              <StyledTableCell align="left">{row.title}</StyledTableCell>
+            </StyledTableRow>
+          ))}
+          <StyledTableRow>
+            <StyledTableCell colspan="3" align="center">
+              30-min Break
+            </StyledTableCell>
+          </StyledTableRow>
+          {section3.map((row) => (
+            <StyledTableRow key={row.title}>
+              <StyledTableCell align="left">{row.time}</StyledTableCell>
+              <StyledTableCell align="left">{row.activity}</StyledTableCell>
+              <StyledTableCell align="left">{row.title}</StyledTableCell>
+            </StyledTableRow>
+          ))}
+          <StyledTableRow>
+            <StyledTableCell colspan="3" align="center">
+              30-min Break
+            </StyledTableCell>
+          </StyledTableRow>
+          {section4.map((row) => (
+            <StyledTableRow key={row.title}>
+              <StyledTableCell align="left">{row.time}</StyledTableCell>
+              <StyledTableCell align="left">{row.activity}</StyledTableCell>
+              <StyledTableCell align="left">{row.title}</StyledTableCell>
+            </StyledTableRow>
+          ))}
+          <StyledTableRow>
+            <StyledTableCell colspan="3" align="center">
+              30-min Break
+            </StyledTableCell>
+          </StyledTableRow>
+          {section5.map((row) => (
+            <StyledTableRow key={row.title}>
+              <StyledTableCell align="left">{row.time}</StyledTableCell>
+              <StyledTableCell align="left">{row.activity}</StyledTableCell>
+              <StyledTableCell align="left">{row.title}</StyledTableCell>
             </StyledTableRow>
           ))}
         </TableBody>
--- src/views/LandingPage/Sections/Schedule.js
+++ src/views/LandingPage/Sections/Schedule.js
@@ -4,18 +4,87 @@ import styles from "assets/jss/material-kit-react/views/landingPageSections/sche
 import CustomizedTables from "components/Table/Table.js";
 import React from "react";
 
-
-
 const useStyles = makeStyles(styles);
 
 export default function Information(props) {
   const classes = useStyles();
   return (
-    <div className={classes.section} style={{textAlign : "left"}}>
+    <div className={classes.section} style={{ textAlign: "left" }}>
       <h2 className={classes.title}>Schedule</h2>
-      <p style={{height: 10}}></p>
+      <p style={{ height: 10 }}></p>
       <CustomizedTables />
-      <p style={{height: 30}}></p>
+
+      <h5 className={classes.title}>Paper presentation - session 1</h5>
+      <ul style={{ color: "#696969", lineHeight: "30px" }}>
+        <li>
+          S3PRL-VC: Open-source Voice Conversion Framework with Self-supervised
+          Speech Representations
+        </li>
+        <li>Pronunciation Adaptive Self Speaking Agent Using WaveGrad</li>
+        <li>
+          Mandarin-English Code-Switching Speech Recognition with
+          Self-Supervised Speech Representation Models
+        </li>
+        <li>
+          Don't speak too fast: The impact of data bias on self-supervised
+          speech models
+        </li>
+      </ul>
+      <h5 className={classes.title}>Paper presentation - session 2</h5>
+      <ul style={{ color: "#696969", lineHeight: "30px" }}>
+        <li>
+          Characterizing the Adversarial Vulnerability of Speech Self-Supervised
+          Learning
+        </li>
+        <li>
+          Membership Inference Attacks Against Self-supervised Speech Models
+        </li>
+        <li>
+          Pretext Tasks Selection for Multitask Self-Supervised Speech
+          Representation
+        </li>
+        <li>
+          DeLoRes: Decorrelating Latent Spaces for Low-Resource Audio
+          Representation Learning
+        </li>
+      </ul>
+      <h5 className={classes.title}>Paper presentation - session 3</h5>
+      <ul style={{ color: "#696969", lineHeight: "30px" }}>
+        <li>
+          An Overview of Unsupervised Representation Learning for Speech, Lasse
+          Borgholt
+        </li>
+        <li>
+          Do self-supervised speech models develop human-like perception biases
+        </li>
+        <li>
+          Investigation on instance mixup regularization strategies for
+          self-supervised speaker representation learning
+        </li>
+        <li>
+          Detecting Depression with a Temporal Context of Speaker Embeddings
+        </li>
+      </ul>
+      <h5 className={classes.title}>Paper presentation (SUPERB)</h5>
+      <ul style={{ color: "#696969", lineHeight: "30px" }}>
+        <li>SUPERB introduction and hidden set evaluation results</li>
+        <li>
+          Self-Supervised Representation Learning for Speech Using Visual
+          Grounding and Masked Language Modeling
+        </li>
+        <li>
+          Invitation talk: WavLM: Large-Scale Self-Supervised Pre-Training for
+          Full Stack Speech Processing
+        </li>
+        <li>
+          DistilHuBERT: Speech Representation Learning by Layer-wise
+          Distillation of Hidden-unit BERT
+        </li>
+        <li>
+          Speech Representation Learning through Self-supervised Pretraining and
+          Multi-task Finetuning
+        </li>
+      </ul>
     </div>
   );
 }
