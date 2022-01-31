bb73355095e501ab6420f2bf5b713b864fb21fcf
KaiWei <ga642381@gmail.com>
2022-01-31 23:01:30 +0800

    Updates

---
+++ precache-manifest.e31a2a9d90e0985e7ed785582dca48f2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7d2a757290ede44a94221dc0526661d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "239683f8746ef5c613d7",
+    "url": "/static/css/main.7422a277.chunk.css"
+  },
+  {
+    "revision": "e257b87f7f8022e44f34",
+    "url": "/static/js/2.c3da7e8d.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.c3da7e8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "239683f8746ef5c613d7",
+    "url": "/static/js/main.7ff55aba.chunk.js"
+  },
+  {
+    "revision": "deaf17d855cbdaf7344d",
+    "url": "/static/js/runtime-main.47788c5d.js"
+  },
+  {
+    "revision": "2ab7ff0a1c469c0d9cdc5c876810d13e",
+    "url": "/static/media/location.2ab7ff0a.jpg"
+  }
+]);
